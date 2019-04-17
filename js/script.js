467cb344941045bd17e276b56f926e5f512b3078
Fatma84 <49618735+Fatma84@users.noreply.github.com>
2019-04-17 13:06:10 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,69 @@
+$(document).ready(function(){
+
+
+$('.owl-carousel').owlCarousel({
+    rtl:true,
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+})
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:5,
+            nav:true,
+            loop:false
+        }
+    }
+})
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+   
+// $('.nav-bar .collapse .navbar-nav .nav-item .nav-link').click(function(){
+//     ('html, body').animate({
+//         scrollTop:
+//         $('.about-us').offset().top},5000 )
+//     })
+
+
+      
+
+});
